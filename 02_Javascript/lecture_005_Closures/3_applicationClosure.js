1789ec7465fa5adb79b9961a59b6e3bd654b8c93
Rajneesh <rajneeshkumar146@gmail.com>
2025-02-19 23:03:58 +0530

    update

--- 02_Javascript/lecture_005_Closures/3_applicationClosure.js
+++ 02_Javascript/lecture_005_Closures/3_applicationClosure.js
@@ -26,14 +26,14 @@ function counter(args) {
 
 // Problem 1: Sum
 function sum(val) {
-    if(val === undefined){
+    if (val === undefined) {
         return 0;
-    }else{
+    } else {
         let res = val;
-        return function smallerSumHelperMethod(val){
-            if(val === undefined){
+        return function smallerSumHelperMethod(val) {
+            if (val === undefined) {
                 return res;
-            }else{
+            } else {
                 res += val;
                 return smallerSumHelperMethod;
             }
@@ -47,19 +47,39 @@ function sum(val) {
 // console.log(sum(3)(7)(8)());  //18
 
 /***************Private variables******/
+
+// Bad code.
+// function createEvenValueStack() {
+//     return {
+//         items: [],
+//         push(item) {
+//             if (item % 2 == 0) {
+//                 console.log("Is pushed");
+//                 this.items.push(item);
+//             } else {
+//                 console.log("Please input even value");
+//             }
+//         },
+//         pop() {
+//             return this.items.pop();
+//         }
+//     };
+// }
+
+// Good code.
 function createEvenValueStack() {
+    let items = [];
     return {
-        items:[],
         push(item) {
             if (item % 2 == 0) {
                 console.log("Is pushed");
-                this.items.push(item);
-            }else {
+                items.push(item);
+            } else {
                 console.log("Please input even value");
             }
         },
-        pop() {  
-            return this.items.pop();
+        pop() {
+            return items.pop();
         }
     };
 }
@@ -71,8 +91,11 @@ stack.push(30);
 stack.push(5);
 console.log("pop ele: ", stack.pop());
 console.log(stack.items);
-// stack.items = [10, 100, 1000]; // prevent this behaviour
+stack.items = [10, 100, 1000]; // prevent this behaviour
+console.log("pop ele: ", stack.pop());
 stack.push(40);
 stack.push(50);
 stack.push(60);
+console.log("pop ele: ", stack.pop());
 console.log(stack.items);
+
