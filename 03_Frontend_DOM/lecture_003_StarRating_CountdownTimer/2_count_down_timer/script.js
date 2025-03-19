eec60dc9604d464b29b1fc24a27e883e405eec83
Rajneesh <rajneeshkumar146@gmail.com>
2025-03-19 22:57:57 +0530

    JS_lastClass

--- 03_Frontend_DOM/lecture_003_StarRating_CountdownTimer/2_count_down_timer/script.js
+++ 03_Frontend_DOM/lecture_003_StarRating_CountdownTimer/2_count_down_timer/script.js
@@ -27,9 +27,116 @@ const hoursInput = document.getElementById("hr");
 const minutesInput = document.getElementById("min");
 const secondsInput = document.getElementById("sec");
 
+let SECOND_IN_HOUR = 3600;
+let SECOND_IN_MINUTES = 60;
+
+let counterId;
+let saveTimeLeft;
+
+resetSetup();
+
+
+
+// EVENT LISTNERS
+startBtn.addEventListener("click", (event) => {
+    // 1. Take inputs and validate it.
+    let hours = getValidInput(hoursInput.value);
+    let minutes = getValidInput(minutesInput.value);
+    let seconds = getValidInput(secondsInput.value);
+
+    if (!validateInputTime(hours, minutes, seconds)) {
+        return;
+    }
+
+    let countDownTimer = hours * SECOND_IN_HOUR + minutes * SECOND_IN_MINUTES + seconds;
+    runTimer(countDownTimer);
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    continueBtn.style.display = "none";
+    resetBtn.style.display = "block";
+});
+
+pauseBtn.addEventListener("click", (e) => {
+
+    // Pause the process.
+    clearInterval(counterId);
+
+    // No change in UI.
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "none";
+    continueBtn.style.display = "block";
+    resetBtn.style.display = "block";
+});
+
+continueBtn.addEventListener("click", (e) => {
+    // Continue the timer.
+    runTimer(saveTimeLeft);
+
+    // No change in UI.
+
+    startBtn.style.display = "none";
+    pauseBtn.style.display = "block";
+    continueBtn.style.display = "none";
+    resetBtn.style.display = "block";
+});
+
+
+
+resetBtn.addEventListener("click", (event) => {
+    resetSetup();
+});
+
 
 
 // Helper Methods.
+function resetSetup() {
+    // Change is UIL: reset UI.
+    hoursInput.value = "00";
+    minutesInput.value = "00";
+    secondsInput.value = "00";
+
+    // reset Process.
+    saveTimeLeft = 0;
+    clearInterval(counterId);
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    continueBtn.style.display = "none";
+    resetBtn.style.display = "none";
+}
+
+function runTimer(countDownTimer) {
+    counterId = setInterval(() => {
+        saveTimeLeft = countDownTimer--;
+
+        updateUiEverySecond(countDownTimer);
+
+        if (countDownTimer < 0) {
+            clearInterval(counterId);
+            return;
+        }
+    }, 1000);
+
+}
+
+function updateUiEverySecond(countDownTimer) {
+    let hours = Math.floor(countDownTimer / SECOND_IN_HOUR);
+    let minutes = Math.floor((countDownTimer % SECOND_IN_HOUR) / SECOND_IN_MINUTES);
+    let seconds = Math.floor(countDownTimer % 60);
+
+    // console.log(hours + ":" + minutes + ":" + seconds);
+    updateUI_Hours_Min_Sec(hours, minutes, seconds);
+}
+
+function updateUI_Hours_Min_Sec(hour, minute, second) {
+    hoursInput.value = hour < 10 ? `0${hour}` : hour;
+    minutesInput.value = minute < 10 ? `0${minute}` : minute;
+    secondsInput.value = second < 10 ? `0${second}` : second;
+
+}
+
 function getValidInput(value) {
     return parseInt(!value ? 0 : value);
 }
