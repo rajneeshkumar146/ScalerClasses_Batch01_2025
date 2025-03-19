e3723bccf24d35f8665c0d30df85992027867844
Rajneesh <rajneeshkumar146@gmail.com>
2025-03-19 21:35:37 +0530

    JS_lastClass

---
+++ 03_Frontend_DOM/lecture_003_StarRating_CountdownTimer/1_star_rating/script.js
@@ -0,0 +1,32 @@
+/**
+ * Javascript:
+           a. We have to listen to three events.
+              i. Click -> Give the rating.
+                          Update star upto that level.
+                          Update rating count.
+
+             ii. Mouseover -> We have to chnage the star upto that level to yellow color without chnaging the rating once rating is given.
+
+             Edge Case:
+             iii. MouseLeave: move it back to gray star/yellow.
+ */
+
+
+const starContainer = document.querySelector(".star_container");
+const countSpan = document.querySelector("#count");
+const starArray = document.querySelectorAll(".star");
+
+let clickedStarIndex = 0;
+let RATING_COLOR = "blue";
+
+
+// Event Listner of click on star.
+
+
+// Event Listner of MouseHover.
+
+
+// Event Listner of MouseLeave.
+
+
+// Helper Methods.
