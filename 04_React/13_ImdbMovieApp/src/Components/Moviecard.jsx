6d9104e0d95d8ccac45d56575c82489da01fbf3d
Rajneesh <rajneeshkumar146@gmail.com>
2025-05-12 22:52:35 +0530

    JS_lastClass

---
+++ 04_React/13_ImdbMovieApp/src/Components/Moviecard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Moviecard() {
+  return (
+    <div>Moviecard</div>
+  )
+}
+
+export default Moviecard
--- 04_React/13_ImdbMovieApp/src/Redux/Movie/MovieMiddleWare.js
+++ 04_React/13_ImdbMovieApp/src/Redux/Movie/MovieMiddleWare.js
@@ -1,1 +1,19 @@
-//   `https://api.themoviedb.org/3/trending/movie/day?api_key=2634e9f079c604567d18059d526b4346&page=1`
+import axios from 'axios';
+import MovieSlice from './MovieSlice';
+
+
+const actions = MovieSlice.actions;
+
+
+export default function MovieMiddleware(pageNo) {
+    return async function (disptach) {
+        try {
+            disptach(actions.setLoading());
+            const res = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=2634e9f079c604567d18059d526b4346&page=${pageNo}`
+            );
+            disptach(actions.setMovies(res.data.results));
+        } catch (err) {
+            disptach(actions.setError());
+        }
+    }
+};
