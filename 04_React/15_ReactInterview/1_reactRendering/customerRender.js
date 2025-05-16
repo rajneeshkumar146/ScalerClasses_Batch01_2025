4e6bbf09c82d6b279448696351fabb8451c49121
Rajneesh <rajneeshkumar146@gmail.com>
2025-05-16 22:54:10 +0530

    ReactSecondLastClass

--- 04_React/15_ReactInterview/1_reactRendering/customerRender.js
+++ 04_React/15_ReactInterview/1_reactRendering/customerRender.js
@@ -2,9 +2,10 @@
 
 <div class="container"> 
     <h1>this is</h1>  
-    <p class="paragraph">I am -> "I am <1 Clicks> from" 
-       <button>1 clicks</button>
-       from 
+    <p class="paragraph">
+       "I am" 
+       <button>1Clicks</button>
+       " from" 
     </p>
 </div> 
 
@@ -24,17 +25,21 @@ const obj =
                 }
             },
             {
-                type: 'p',
+                type: 'p', 
                 props: {
                     class: 'paragraph',
                     children: [
                         ' I am ',
                         {
                             type: (props) => ({
-                                type: "button", props:
-                                    { children: props.counter + "Clicks" }
+                                type: "button", 
+                                props: { 
+                                    children: props.counter + "Clicks" 
+                                }
                             }),
-                            props: { counter: 1 }
+                            props: { 
+                                counter: 1 
+                            }
                         }
                         ,
                         ' from'
@@ -45,8 +50,61 @@ const obj =
     }
 }
 
-function render(obj) {
+/**
+ * 1. type: 
+ *        -> string : html element: document.createElement
+ *        -> function: custom component: call that function and if we have props then pass that also.
+ * 
+ * 2. props:
+ *        -> Object
+ *           -> values -> string: normal attribute example: class, id -> setAttribute
+ *           -> children:
+ *                     -> Array
+ *                     -> can have value as a string.
+ * 
+ *                     -> can have values as a function: custome element
+ * 
+ * 
+ */
 
+function render(obj) {
+    let element;
+    if (typeof obj.type === "string") {
+        element = document.createElement(obj.type);
+    } else if (typeof obj.type === "function") {
+        const prop = obj['props'];
+        let elementObj = obj.type(prop);
+        return render(elementObj);
+    }
+
+
+    const props = obj.props;
+    for (let prop in props) {
+        if (prop === 'children') {
+            const childrenElement = props[prop];
+            let isArray = Array.isArray(childrenElement);
+            if (isArray) {
+                for (let i = 0; i < childrenElement.length; i++) {
+                    let child = childrenElement[i];
+                    if (typeof child === 'string') {
+                        const textNode = document.createTextNode(child);
+                        element.appendChild(textNode);
+                    } else {
+                        const childElem = render(child);
+                        console.log(childElem);
+                        element.appendChild(childElem);
+                    }
+                }
+            } else {
+                const childElem = document.createTextNode(props[prop]);
+                element.appendChild(childElem);
+            }
+        } else if (typeof props[prop] === 'string') {
+            element.setAttribute(prop, props[prop]);
+        }
+    }
+
+    return element;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
